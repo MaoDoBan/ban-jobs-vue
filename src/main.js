50d6095c36fec810f1e89b44617ca01920de512d
MaoDoBan <11332249+MaoDoBan@users.noreply.github.com>
2022-10-31 19:50:16 -0300

    estética, import e mini alterações

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
+import router from './router' //importa automático o index.js da pasta /router
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).mount('#app');
