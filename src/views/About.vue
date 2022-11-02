f6bdf655f51fb0a1f6236cc557ad169acfee17ec
MaoDoBan <11332249+MaoDoBan@users.noreply.github.com>
2022-11-02 09:10:20 -0300

    alterações estéticas e de conteúdo

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam saepe autem inventore rerum doloremque aliquam at quo omnis eaque aliquid molestiae,
+       dolor nulla odit blanditiis impedit suscipit nihil voluptatibus laboriosam quos ipsa. Nostrum praesentium beatae similique nam optio repellat hic,
+       amet doloremque porro laudantium eum corporis expedita sapiente tempore unde dolorem tempora, dolorum vel recusandae ratione! Aliquam vero odit eos in similique quidem?
+       Dolorum similique ea earum illum deserunt iste consequuntur nesciunt ad vitae eum, corporis provident amet!
+       Recusandae sint ratione totam facere quos tempora distinctio earum optio numquam, deleniti, corrupti culpa eos? Blanditiis obcaecati quia nulla architecto dolorum maiores.</p>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homepage</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam saepe autem inventore rerum doloremque aliquam at quo omnis eaque aliquid molestiae,
+       dolor nulla odit blanditiis impedit suscipit nihil voluptatibus laboriosam quos ipsa. Nostrum praesentium beatae similique nam optio repellat hic,
+       amet doloremque porro laudantium eum corporis expedita sapiente tempore unde dolorem tempora, dolorum vel recusandae ratione! Aliquam vero odit eos in similique quidem?
+       Dolorum similique ea earum illum deserunt iste consequuntur nesciunt ad vitae eum, corporis provident amet!
+       Recusandae sint ratione totam facere quos tempora distinctio earum optio numquam, deleniti, corrupti culpa eos? Blanditiis obcaecati quia nulla architecto dolorum maiores.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam saepe autem inventore rerum doloremque aliquam at quo omnis eaque aliquid molestiae,
+       dolor nulla odit blanditiis impedit suscipit nihil voluptatibus laboriosam quos ipsa. Nostrum praesentium beatae similique nam optio repellat hic,
+       amet doloremque porro laudantium eum corporis expedita sapiente tempore unde dolorem tempora, dolorum vel recusandae ratione! Aliquam vero odit eos in similique quidem?
+       Dolorum similique ea earum illum deserunt iste consequuntur nesciunt ad vitae eum, corporis provident amet!
+       Recusandae sint ratione totam facere quos tempora distinctio earum optio numquam, deleniti, corrupti culpa eos? Blanditiis obcaecati quia nulla architecto dolorum maiores.</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+  components: {}
 }
 </script>
