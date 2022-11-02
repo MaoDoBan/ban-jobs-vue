52b0c16219b02a7a345efd791a9e6da48400a31b
MaoDoBan <11332249+MaoDoBan@users.noreply.github.com>
2022-11-02 13:58:24 -0300

    +Jobs.vue página

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <nav>
     <router-link :to="{ name: 'home' }">Home</router-link> |
+    <router-link :to="{ name: 'jobs' }">Jobs</router-link> |
     <router-link :to="{ name: 'about' }">About</router-link>
   </nav>
   <router-view/>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/Home.vue'
 import AboutView from '../views/About.vue'
+import JobsView from '../views/Jobs.vue'
 
 const routes = [
   {
@@ -12,6 +13,11 @@ const routes = [
     path: '/about',
     name: 'about',
     component: AboutView
+  },
+  {
+    path: '/jobs',
+    name: 'jobs',
+    component: JobsView
   }
 ];
 
