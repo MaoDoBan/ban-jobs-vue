4bb3adaf5ffa392df7b0814cf071a688afdd45cf
MaoDoBan <11332249+MaoDoBan@users.noreply.github.com>
2022-11-03 15:53:02 -0300

    Parâmetros de rots

--- src/views/jobs/JobDetails.vue
+++ src/views/jobs/JobDetails.vue
@@ -1,10 +1,15 @@
 <template>
-  
+  <h1>Detalhes da vaga de emprego</h1>
+  <p>O id é: {{ id }}</p>
 </template>
 
 <script>
 export default {
-
+  data(){
+    return{
+      id: this.$route.params.id
+    }
+  }
 }
 </script>
 
