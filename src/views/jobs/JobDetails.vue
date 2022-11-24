e91a4584967377795767b5f74cf0b255abd054c2
MaoDoBan <11332249+MaoDoBan@users.noreply.github.com>
2022-11-24 17:00:36 -0300

    fetch do job no JobDetails

--- src/views/jobs/JobDetails.vue
+++ src/views/jobs/JobDetails.vue
@@ -1,11 +1,23 @@
 <template>
-  <h1>Detalhes da vaga de emprego</h1>
+  <h1>{{ job.title }}</h1>
   <p>O id é: {{ id }}</p>
+  <p>{{ job.details }}</p>
 </template>
 
 <script>
 export default {
-  props: ["id"]
+  props: ["id"],
+  data(){
+    return {
+      job: {title: "", details: ""}
+    }
+  },
+  mounted(){
+    fetch("http://localhost:3000/jobs/" + this.id)
+      .then(res => res.json()) //res.json é assíncrono também
+      .then(data => this.job = data)
+      .catch(erro => console.log(erro.message));
+  }
 }
 </script>
 
